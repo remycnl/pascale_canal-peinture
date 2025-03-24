78d271badf31e267a0903de9d50882caab12824d
Rémy Canal <remy.canal@epitech.eu>
2025-03-24 15:38:26 +0100

    [responsive design] footer

--- plugins/darkMode.js
+++ plugins/darkMode.js
@@ -3,27 +3,27 @@ import { useDarkMode } from "@/composables/useDarkMode";
 import { useRoute } from "vue-router";
 
 export default defineNuxtPlugin(() => {
-    const { isDarkMode } = useDarkMode();
-    const route = useRoute();
+	const { isDarkMode } = useDarkMode();
+	const route = useRoute();
 
-    const updateBodyStyles = (darkMode) => {
-        if (import.meta.client) {
-            if (darkMode) {
-                document.body.style.backgroundColor = 'var(--color-black)';
-                document.body.style.color = 'var(--color-white)';
-            } else {
-                document.body.style.backgroundColor = 'var(--color-white)';
-                document.body.style.color = 'var(--color-black)';
-            }
-        }
-    };
-    
-    watch(
-        () => route.path,
-        (newPath) => {
-            isDarkMode.value = newPath === "/qui-suis-je" || newPath === "/contact";
-            updateBodyStyles(isDarkMode.value);
-        },
-        { immediate: true }
-    );
+	const updateBodyStyles = (darkMode) => {
+		if (import.meta.client) {
+			if (darkMode) {
+				document.body.style.backgroundColor = "var(--color-black)";
+				document.body.style.color = "var(--color-white)";
+			} else {
+				document.body.style.backgroundColor = "var(--color-white)";
+				document.body.style.color = "var(--color-black)";
+			}
+		}
+	};
+
+	watch(
+		() => route.path,
+		(newPath) => {
+			isDarkMode.value = newPath === "/qui-suis-je" || newPath === "/contact";
+			updateBodyStyles(isDarkMode.value);
+		},
+		{ immediate: true }
+	);
 });
