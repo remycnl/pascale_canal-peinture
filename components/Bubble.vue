eaae94bc07b1aef6296a7c91423c08e0710d5059
Rémy Canal <remy.canal@epitech.eu>
2025-03-24 01:39:25 +0100

    [responsive design] bubble

--- components/Bubble.vue
+++ components/Bubble.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, onMounted } from "vue";
+import { ref, onMounted, computed } from "vue";
 import { useRouter } from "vue-router";
 
 const router = useRouter();
@@ -8,6 +8,27 @@ const suggestions = ref([]);
 const allTables = ref([]);
 const isInputFocused = ref(false);
 const preventBlurEvent = ref(false);
+const isMobile = ref(false);
+
+const checkScreenSize = () => {
+	isMobile.value = window.innerWidth < 768;
+};
+
+const bubbleWidthClass = computed(() => {
+	if (isMobile.value) {
+		return "w-full";
+	} else {
+		return isInputFocused.value ? "w-150" : "w-120";
+	}
+});
+
+const bubblePaddingClass = computed(() => {
+	if (isMobile.value) {
+		return "px-1.5";
+	} else {
+		return isInputFocused.value ? "pl-8" : "pl-4";
+	}
+});
 
 // Fonction pour remonter en haut de la page
 const scrollToTop = () => {
@@ -84,49 +105,65 @@ onMounted(async () => {
 	} catch (error) {
 		console.error("Erreur lors de la récupération des tableaux:", error);
 	}
+
+	// Initialiser et surveiller les changements de taille d'écran
+	checkScreenSize();
+	window.addEventListener("resize", checkScreenSize);
 });
 </script>
 
 <template>
 	<div class="flex justify-center w-full">
 		<div
-			:class="{
-				'w-150 pl-8 hover:scale-100': isInputFocused,
-				'w-120 pl-4 hover:scale-105': !isInputFocused,
-			}"
-			class="z-50 h-fit origin-center bg-[#000000] rounded-full fixed bottom-5 py-2 pr-2 transition-all duration-500">
-			<div class="flex justify-between items-center space-x-2">
-				<NuxtImg src="/img/logo-reversed.png" alt="Logo" class="w-auto h-7" />
-
-				<div class="relative ml-10 flex-grow">
-					<input
-						v-model="searchQuery"
-						@keyup.enter="navigateToTable"
-						@input="searchTables"
-						@focus="isInputFocused = true"
-						@blur="handleBlur"
-						placeholder="Rechercher un tableau..."
-						class="w-full px-3 py-2 text-sm text-gray-200 bg-black rounded-full focus:outline-none" />
-					<div
-						v-if="suggestions.length > 0"
-						class="absolute w-full bottom-full p-2 mb-3 bg-[#000000] rounded-2xl z-10">
-						<ul>
-							<li
-								v-for="suggestion in suggestions"
-								:key="suggestion.id"
-								@click="selectSuggestion(suggestion)"
-								@mousedown="preventBlur"
-								class="px-3 py-2 text-sm text-white hover:bg-black rounded-lg cursor-pointer">
-								{{ suggestion.name }}
-							</li>
-						</ul>
-					</div>
-				</div>
-
+			:class="[
+				bubbleWidthClass,
+				bubblePaddingClass,
+				{
+					'hover:scale-100': isInputFocused,
+					'hover:scale-105': !isInputFocused && !isMobile,
+					'max-w-screen-sm mx-4': isMobile,
+				},
+			]"
+			class="z-50 h-fit origin-center bg-[#000000] rounded-full fixed bottom-5 py-1.5 md:py-2 pr-1.5 md:pr-2 transition-all duration-500">
+			<div class="flex justify-between items-center gap-x-5 sm:gap-x-10 md:gap-x-8">
+				<NuxtImg
+					src="/img/logo-reversed.png"
+					alt="Logo"
+					class="w-auto ml-2 md:ml-0 h-6 md:h-7"
+					:class="{ hidden: isMobile && isInputFocused }" />
 				<div
-					class="text-black text-sm px-5 py-2 bg-yellow hover:bg-yellow/85 active:scale-95 rounded-full cursor-pointer transition-all duration-300"
-					@click="scrollToTop">
-					Retour en haut
+					class="relative flex justify-between items-center gap-x-2 flex-grow">
+					<div class="relative flex-grow">
+						<input
+							v-model="searchQuery"
+							@keyup.enter="navigateToTable"
+							@input="searchTables"
+							@focus="isInputFocused = true"
+							@blur="handleBlur"
+							placeholder="Rechercher un tableau..."
+							class="w-full px-3 py-1.5 md:py-2 text-sm text-gray-200 bg-black rounded-full focus:outline-none" />
+						<div
+							v-if="suggestions.length > 0"
+							class="absolute w-full bottom-full p-2 mb-3 bg-[#000000] rounded-2xl z-10">
+							<ul>
+								<li
+									v-for="suggestion in suggestions"
+									:key="suggestion.id"
+									@click="selectSuggestion(suggestion)"
+									@mousedown="preventBlur"
+									class="px-3 py-2 text-sm text-white hover:bg-black rounded-lg cursor-pointer">
+									{{ suggestion.name }}
+								</li>
+							</ul>
+						</div>
+					</div>
+
+					<div
+						class="hidden md:block text-black md:text-sm px-3 md:px-5 py-2 bg-yellow hover:bg-yellow/85 active:scale-95 rounded-full cursor-pointer transition-all duration-300 whitespace-nowrap"
+						:class="{ hidden: isMobile && isInputFocused }"
+						@click="scrollToTop">
+						Retour en haut
+					</div>
 				</div>
 			</div>
 		</div>
@@ -137,5 +174,12 @@ onMounted(async () => {
 ::selection {
 	background-color: var(--color-white);
 	color: var(--color-black);
+}
+
+@media (max-width: 640px) {
+	.z-50 {
+		width: calc(100% - 2rem);
+		max-width: 100%;
+	}
 }
 </style>
