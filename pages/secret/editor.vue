d686f3ee4acd29e3fe502fda155edcf41ef78adc
Rémy Canal <remy.canal@epitech.eu>
2025-03-25 15:40:15 +0100

    [fix] password modal

--- pages/secret/editor.vue
+++ pages/secret/editor.vue
@@ -542,7 +542,7 @@ const resetForm = () => {
 							@mouseup.prevent="showPassword = false"
 							@mouseleave="showPassword = false"
 							@touchstart.prevent="showPassword = !showPassword"
-							class="absolute right-4 top-1/2 -translate-y-[90%] text-gray-500 hover:text-gray-700 focus:outline-none">
+							class="absolute right-4 top-1/2 -translate-y-[80%] lg:-translate-y-[90%] text-gray-500 hover:text-gray-700 focus:outline-none">
 							<NuxtImg
 								v-if="!showPassword"
 								src="/svg/eye-open.svg"
