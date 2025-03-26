0650a33872abf15b7c63edd7da4460915e879308
Rémy Canal <remy.canal@epitech.eu>
2025-03-27 00:31:49 +0100

    [fix] router on slug page

--- pages/[slug].vue
+++ pages/[slug].vue
@@ -121,7 +121,7 @@ useSeoMeta({
 	<div class="relative min-h-screen pt-10">
 		<div v-if="painting">
 			<button
-				@click="$router.back()"
+				@click="$router.back() || $router.push('/')"
 				title="Go to the previous page"
 				class="rotate-180 active:scale-95 w-10 h-10 md:w-15 md:h-15 lg:w-20 lg:h-20 pointer-cursor hover:-translate-x-2 transition-transform duration-200">
 				<NuxtImg src="/svg/arrow-black.svg" />
