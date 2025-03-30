ecc3c446220b27b8a861d3c6ce6d4640e7582822
Rémy Canal <remy.canal@epitech.eu>
2025-03-30 03:26:18 +0200

    [fix] contact page

--- pages/[slug].vue
+++ pages/[slug].vue
@@ -382,70 +382,71 @@ useSchemaOrg([
 		<Teleport to="body">
 			<!-- Contact Overlay -->
 			<Transition name="overlay">
-			<div
-				v-if="showContactOverlay"
-				class="fixed inset-0 backdrop-blur-md w-screen h-screen z-[9999] flex items-center justify-center">
-				<!-- Backdrop avec flou -->
+				<div
+					v-if="showContactOverlay"
+					class="fixed inset-0 backdrop-blur-md w-screen h-screen z-[9999] flex items-center justify-center">
+					<!-- Backdrop avec flou -->
 					<div
 						class="absolute inset-0 bg-black/70"
 						@click="closeContactOverlay"></div>
-				
 
-				<!-- Modal -->
-				<div
-					class="relative bg-black rounded-4xl w-fit m-1 sm:m-3 md:m-10 max-h-[99vh] md:max-h-[90vh] z-10 pt-6 p-4 sm:p-6 md:p-8 lg:p-10 flex flex-col overflow-y-auto transform transition-all duration-400 ease-out"
-					:class="{
-						'translate-y-8 opacity-0 scale-95': !formLoaded,
-						'translate-y-0 opacity-100 scale-100': formLoaded,
-					}">
-					<!-- Bouton de fermeture -->
-					<button
-						@click="closeContactOverlay"
-						class="cross-button absolute top-4 right-4 text-gray-400 hover:text-white transition-colors duration-200">
-						<svg
-							class="h-10 w-10 md:h-12 md:w-12 lg:h-15 lg:w-15"
-							viewBox="0 0 24 24"
-							fill="none"
-							xmlns="http://www.w3.org/2000/svg">
-							<path
-								d="M16 8L8 16M12 12L16 16M8 8L10 10"
-								stroke="currentColor"
-								stroke-width="1.5"
-								stroke-linecap="round"
-								stroke-linejoin="round" />
-						</svg>
-					</button>
+					<!-- Modal -->
+					<div
+						class="relative bg-black rounded-4xl w-fit m-1 sm:m-3 md:m-10 max-h-[99vh] md:max-h-[90vh] z-10 pt-6 p-4 sm:p-6 md:p-8 lg:p-10 flex flex-col overflow-y-auto transform transition-all duration-400 ease-out"
+						:class="{
+							'translate-y-8 opacity-0 scale-95': !formLoaded,
+							'translate-y-0 opacity-100 scale-100': formLoaded,
+						}">
+						<!-- Bouton de fermeture -->
+						<button
+							@click="closeContactOverlay"
+							class="cross-button absolute top-4 right-4 text-gray-400 hover:text-white transition-colors duration-200">
+							<svg
+								class="h-10 w-10 md:h-12 md:w-12 lg:h-15 lg:w-15"
+								viewBox="0 0 24 24"
+								fill="none"
+								xmlns="http://www.w3.org/2000/svg">
+								<path
+									d="M16 8L8 16M12 12L16 16M8 8L10 10"
+									stroke="currentColor"
+									stroke-width="1.5"
+									stroke-linecap="round"
+									stroke-linejoin="round" />
+							</svg>
+						</button>
 
-					<!-- Contenu du modal -->
-					<h2 class="text-2xl md:text-3xl font-bold text-white mb-6">
-						{{
-							contactType === "achat"
-								? "Acheter cette œuvre"
-								: "Demander une réédition"
-						}}
-					</h2>
-					<p class="mb-6 max-w-5xl text-gray-400">
-						<span v-if="contactType === 'achat'">
-							Vous êtes intéressé(e) par l'achat de "{{ painting.name }}" ?
-							Remplissez ce formulaire et je vous contacterai rapidement pour
-							discuter des détails.
-						</span>
-						<span v-else>
-							Vous souhaitez une réédition de "{{ painting.name }}" ? Chaque
-							œuvre étant unique, la nouvelle version sera inspirée de
-							l'originale mais présentera ses propres particularités. Remplissez
-							ce formulaire et je vous contacterai rapidement pour discuter du
-							projet.
-						</span>
-					</p>
-					<div class="flex-grow overflow-auto">
-						<ContactForm
-							:pre-selected-artwork-id="painting.id"
-							@form-loaded="formLoaded = true" />
+						<!-- Contenu du modal -->
+						<h2 class="text-2xl md:text-3xl font-bold text-white mb-6">
+							{{
+								contactType === "achat"
+									? "Acheter cette œuvre"
+									: "Demander une réédition"
+							}}
+						</h2>
+						<p class="mb-6 max-w-5xl text-gray-400">
+							<span v-if="contactType === 'achat'">
+								Vous êtes intéressé(e) par l'achat de "{{ painting.name }}" ?
+								Remplissez ce formulaire et je vous contacterai rapidement pour
+								discuter des détails.
+							</span>
+							<span v-else>
+								Vous souhaitez une réédition de "{{ painting.name }}" ?
+								<span class="hidden md:inline">
+									Chaque œuvre étant unique, la nouvelle version sera inspirée
+									de l'originale mais présentera ses propres particularités.
+								</span>
+								Remplissez ce formulaire et je vous contacterai rapidement pour
+								discuter du projet.
+							</span>
+						</p>
+						<div class="flex-grow overflow-auto">
+							<ContactForm
+								:pre-selected-artwork-id="painting.id"
+								@form-loaded="formLoaded = true" />
+						</div>
 					</div>
 				</div>
-			</div>
-		</Transition>
+			</Transition>
 		</Teleport>
 	</div>
 </template>
